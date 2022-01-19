8a758cd2d155065d714fb1fb1ec4b1f03ac9c340
skywind2002 <wuhc19@mails.tsinghua.edu.cn>
2022-01-19 16:05:51 +0800

    模块化之前代码

--- KernelLoss.m
+++ KernelLoss.m
@@ -1,5 +1,11 @@
-function loss=KernelLoss(x,y,param,name)
-  % 计算当前参数的Loss函数，以此作为优化的标准
-  K=KernelFunc(x,x,param,name)+1e-8*eye(length(x));  % 1e-8为噪声大小
-  loss=y*inv(K)*y'/2+log(abs(det(K)))/2;
+function loss = KernelLoss(x, y, param, name)
+    % 计算当前参数的Loss函数，以此作为优化的标准
+    K = KernelFunc(x, x, param, name) + 1e-8 * eye(length(x)); % 1e-8为噪声大小
+
+    if (det(K) > 0 && det(K) < Inf)
+        loss = y' * inv(K) * y / 2 + log(abs(det(K))) / 2;
+    else
+        loss = 1e10;
+    end
+
 end
--- main.m
+++ main.m
@@ -1,30 +1,42 @@
-filename="1M.csv";
-filepath="./data/"+filename;
-split_ratio=0.5;
+clc;
+clear;
 
-kernel_name='lpk';   %有eqk,pk,lpk三种选择，具体来源https://peterroelants.github.io/posts/gaussian-process-kernels/
+% hyperparam  超参数抉择，主要有数据来源，参数搜索方式，组数，核函数
+filename = "1Y.csv";
+filepath = "./data/" + filename;
+split_ratio = 0.1;
+
+logspace_flag = 1; %参数搜索时是否采用对数空间
+argsnum = 10; %参数搜索组数
+kernel_name = 'eqk'; %有eqk,pk,lpk三种选择，具体来源https://peterroelants.github.io/posts/gaussian-process-kernels/
 % 参考文献A tutorial on Gaussian process regression: Modelling, exploring, and exploiting functions[J]. Journal of Mathematical Psychology, 2018, 85:1-16
 
-switch kernel_name  %根据不同的核函数选择初始参数
-    case "eqk"  %平方指数核的参数[l,sigma],以及对应的搜索上下限
-        param=[1.16,12];
-        bound=[1e-1,1e3,1e-1,1e2];
+switch kernel_name %根据不同的核函数选择初始参数
+    case "eqk" %平方指数核的参数[l,sigma],以及对应的搜索上下限
+        paramInit = [1.16, 12];
+        bound = [1e-1, 1e3, 1e-1, 1e2];
     case "pk"
-        param=[1.16,12,10];  %周期核的参数[l,sigma,p],以及对应的搜索上下限
-        bound=[1e-1,1e3,1e-1,1e3,5,1e2];
+        paramInit = [1.16, 12, 10]; %周期核的参数[l,sigma,p],以及对应的搜索上下限
+        bound = [1e-1, 1e3, 1e-1, 1e3, 5, 1e2];
     case "lpk"
-        param=[1.12,0.1,19,16.1];  %局部周期核的参数[lse,lp,p,sigma],以及对应的搜索上下限
-        bound=[1e-1,1e3,1e-1,1e3,5,1e3,1e-1,1e3];
+        paramInit = [1.12, 0.1, 19, 16.1]; %局部周期核的参数[lse,lp,p,sigma],以及对应的搜索上下限
+        bound = [1e-1, 1e3, 1e-1, 1e3, 5, 1e3, 1e-1, 1e3];
     otherwise
         disp("核函数错误!");
-        return;  %终止整个程序的运行
+        return; %终止整个程序的运行
 end
 
-[x_train,x_test,y_train,y_test]=DataProcess(filepath,split_ratio);
+%preprocessing  分离测试集和训练集
+[x_train, x_test, y_train, y_test] = DataProcess(filepath, split_ratio);
+y = [y_train; y_test];
 
-a=[1,2];
-b=[2,3];
-loss=KernelLoss(a,b,param,kernel_name);
+%train  蛮力法获取最优参数
+param = Train(x_train, y_train, paramInit, argsnum, bound, logspace_flag, kernel_name);
 
+%predict 根据最优参数进行预测
+[mu_test, cov_test, sigma_test] = Predict(x_train, y_train, x_test, param, kernel_name); %长期预测，效果不佳
+%[mu_test,sigma_test]=ShortPredict(x_train,y_train,x_test,y,param,kernel_name);
+%短期预测 效果良好
 
-
+%draw  画图
+DrawPlot(mu_test, sigma_test, x_train, y_train, x_test, y_test);
